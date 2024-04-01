12b256eeb140b45bc09a5cb4d21e92e8f9cf50db
Ewout van der Veer <ewoutvanderveer@msn.com>
2024-04-01 23:59:48 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd $HOME
+TMPDIR=$(mktemp -d -t .)
+
+sudo apt install git gh zsh feh kitty
+
+if ! command -v nvim &>/dev/null; then
+	#Install Neovim
+	sudo apt install cmake ninja-build gettext curl unzip build-essential
+	git clone https://github.com/neovim/neovim
+	cd neovim
+	make CMAKE
+fi
+
+#Get dotfiles
+git clone https://github.com/evanderveer/dotfiles $HOME/.dotfiles
+
+cd $HOME
+rm -rf $TMPDIR
