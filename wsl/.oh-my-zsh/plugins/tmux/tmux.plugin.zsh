3fda02d2f3e38df0f035209261f4f92dfae9f40a
Ewout van der Veer <ewoutvanderveer@msn.com>
2025-06-09 22:35:08 +0200

    update Oh-My-Zsh

--- wsl/.oh-my-zsh/plugins/tmux/tmux.plugin.zsh
+++ wsl/.oh-my-zsh/plugins/tmux/tmux.plugin.zsh
@@ -13,6 +13,10 @@ fi
 : ${ZSH_TMUX_AUTOCONNECT:=true}
 # Automatically close the terminal when tmux exits
 : ${ZSH_TMUX_AUTOQUIT:=$ZSH_TMUX_AUTOSTART}
+# Automatically name the new session based on the basename of PWD
+: ${ZSH_TMUX_AUTONAME_SESSION:=false}
+# Automatically pick up tmux environments
+: ${ZSH_TMUX_AUTOREFRESH:=false}
 # Set term to screen or screen-256color based on current terminal support
 : ${ZSH_TMUX_DETACHED:=false}
 # Set detached mode
@@ -48,6 +52,7 @@ fi
 
 # ALIASES
 function _build_tmux_alias {
+  setopt localoptions no_rc_expand_param
   eval "function $1 {
     if [[ -z \$1 ]] || [[ \${1:0:1} == '-' ]]; then
       tmux $2 \"\$@\"
@@ -55,6 +60,19 @@ function _build_tmux_alias {
       tmux $2 $3 \"\$@\"
     fi
   }"
+
+  local f s
+  f="_omz_tmux_alias_${1}"
+  s=(${(z)2})
+
+  eval "function ${f}() {
+    shift words;
+    words=(tmux ${@:2} \$words);
+    ((CURRENT+=${#s[@]}+1))
+    _tmux
+  }"
+
+  compdef "$f" "$1"
 }
 
 alias tksv='tmux kill-server'
@@ -102,9 +120,22 @@ function _zsh_tmux_plugin_run() {
 
   local _detached=""
   [[ "$ZSH_TMUX_DETACHED" == "true" ]] && _detached="-d"
+
+  local session_name
+  if [[ "$ZSH_TMUX_AUTONAME_SESSION" == "true" ]]; then
+    # Name the session after the basename of the current directory
+    session_name=${PWD##*/}
+    # If the current directory is the home directory, name it 'HOME'
+    [[ "$PWD" == "$HOME" ]] && session_name="HOME"
+    # If the current directory is the root directory, name it 'ROOT'
+    [[ "$PWD" == "/" ]] && session_name="ROOT"
+  else
+      session_name="$ZSH_TMUX_DEFAULT_SESSION_NAME"
+  fi
+
   # Try to connect to an existing session.
-  if [[ -n "$ZSH_TMUX_DEFAULT_SESSION_NAME" ]]; then
-    [[ "$ZSH_TMUX_AUTOCONNECT" == "true" ]] && $tmux_cmd attach $_detached -t $ZSH_TMUX_DEFAULT_SESSION_NAME
+  if [[ -n "$session_name" ]]; then
+    [[ "$ZSH_TMUX_AUTOCONNECT" == "true" ]] && $tmux_cmd attach $_detached -t "$session_name"
   else
     [[ "$ZSH_TMUX_AUTOCONNECT" == "true" ]] && $tmux_cmd attach $_detached
   fi
@@ -116,8 +147,9 @@ function _zsh_tmux_plugin_run() {
     elif [[ -e "$ZSH_TMUX_CONFIG" ]]; then
       tmux_cmd+=(-f "$ZSH_TMUX_CONFIG")
     fi
-    if [[ -n "$ZSH_TMUX_DEFAULT_SESSION_NAME" ]]; then
-      $tmux_cmd new-session -s $ZSH_TMUX_DEFAULT_SESSION_NAME
+
+    if [[ -n "$session_name" ]]; then
+      $tmux_cmd new-session -s "$session_name"
     else
       $tmux_cmd new-session
     fi
@@ -126,6 +158,15 @@ function _zsh_tmux_plugin_run() {
   if [[ "$ZSH_TMUX_AUTOQUIT" == "true" ]]; then
     exit
   fi
+}
+
+# Refresh tmux environment variables.
+function _zsh_tmux_plugin_preexec()
+{
+  local -a tmux_cmd
+  tmux_cmd=(command tmux)
+
+  eval $($tmux_cmd show-environment -s)
 }
 
 # Use the completions for tmux for our function
@@ -153,4 +194,10 @@ if [[ -z "$TMUX" && "$ZSH_TMUX_AUTOSTART" == "true" && -z "$INSIDE_EMACS" && -z 
     export ZSH_TMUX_AUTOSTARTED=true
     _zsh_tmux_plugin_run
   fi
+fi
+
+# Automatically refresh tmux environments if tmux is running.
+if [[ -n "$TMUX" && "$ZSH_TMUX_AUTOREFRESH" == "true" ]] && tmux ls >/dev/null 2>/dev/null; then
+  autoload -U add-zsh-hook
+  add-zsh-hook preexec _zsh_tmux_plugin_preexec
 fi
